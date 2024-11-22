c498f6bb4b55bcb2aa8dc018737eea4df89f33ae
nam-chu <74799165+nam-chu@users.noreply.github.com>
2024-11-22 12:27:03 -0500

    implementing model diagnostics

--- 3553-project-code-nam.R
+++ 3553-project-code-nam.R
@@ -19,7 +19,7 @@ w_rug<- read_excel(path="~/3553-final-project/unclean-data/cmj/Women's Rugby_CMJ
 
 # Cleaning data -----------------------------------------------------------
 
-# Here I've used the maggritr pipe (|> or %>%) operator from the dyplr package. This allows me to efficiently declare cleaning operators. It can be read as "and then". I've also used a flurry of dplyr subsetting functions that allow me to perform data cleaning operations. 
+# Here I've used the maggritr pipe (|> or %>%) operator from the dyplr package. This allows me to efficiently declare commands. It can be read as "and then". I've also used a flurry of dplyr subsetting functions that allow me to perform data cleaning operations. 
 
 # The first cleaning operation can be read the following: 
 
@@ -89,20 +89,102 @@ data <- bind_rows(m_basket, w_basket, m_soc, w_soc, m_hock, w_hock, w_rug, m_foo
 rm(m_basket, w_basket, m_soc, w_soc, m_hock, w_hock, w_rug, m_foot)
 
 
-# Multicollinearity Checks ------------------------------------------------
+# Multi-collinearity Checks ------------------------------------------------
 
-# Here I've subsetted our large dataset into a smaller one that contains only the columns from sex onwards. Clearly we don't need the jump id and so forth. 
+# Here I've subsetted our large data set into a smaller one that contains only the columns from sex on wards. Clearly we don't need the jump id and so forth. 
 data2 <- data |> 
-  select(sex:`Relative Propulsive Net Impulse`) 
+  select(sex:`Relative Propulsive Net Impulse`)
 
+data2$sex
+  
 # I'm rearranging the columns here so that they are listed alphabetically to better match the data dictionary
 data2 <- data2 |> 
+  mutate(
+    sex = as.factor(sex), # 
+    sport = as.factor(sport)
+  ) |> 
   select(order(colnames(data2))) |> 
-  relocate(where(is.numeric), .after = where(is.character)) 
+  relocate(where(is.numeric), .after = where(is.factor)) 
 
-mod_1 <- lm(`Jump Height` ~ ., data = data2[, !(names(data2) %in% c("Left Force at Peak Propulsive Force", "Left Avg. Propulsive Force", "Left Force at Peak Landing Force", "Left Force at Peak Braking Force", "Left Avg. Landing Force", "Left Avg. Braking Force", "Left Avg. Braking RFD", "Right Force at Peak Propulsive Force", "Right Force at Peak Landing Force", "Right Force at Peak Braking Force", "Right Avg. Propulsive Force", "Right Avg. Propulsive Force", "Right Avg. Landing Force", "Right Avg. Braking RFD", "Right Avg. Braking Force", "sex", "sport"))]) # lm(y ~ . , data=data) the . allows you to fit all predictors in your dataframe without having to explicitly type them all out. 
+# This is the correlation matrix 
+cor_matrix <- cor(data2[3:81], use="complete.obs")
 
-summary(mod_1) # This is the summary of the model.
+# Model Diagnostics -------------------------------------------------------
 
-vif(mod_1)
+data2 <- na.omit(data2)
+mod_1 <- lm(`Jump Height` ~ ., data = data2)
+mod_residuals <- residuals(mod_1)
+mod_fitted <- fitted(mod_1)
+
+length(mod_residuals)
+length(mod_fitted)
+
+plot <- ggplot(data2, aes(x = mod_fitted, y = mod_residuals)) +
+                 geom_point() + xlab("Model Fitted Values") +ylab("Model Residuals") + 
+                 geom_hline(yintercept = 0)
+plot
+
+# Check the normality assumption ------------------------------------------
+
+qqnorm(mod_residuals,
+       ylab = "Residuals",
+       main = "",
+       lwd = 2)
+qqline(mod_residuals) # clearly this doesn't look normal. 
+
+shapiro.test(mod_residuals) # This won't work because Shapiro Test only takes sample size between 3 and 5000. 
+
+
+# LASSO  -----------------------------------------------------------------
+
+
+
+
+# PCA -------------------------------------------------------------------
+
+# Multi-Collinearity Checks -----------------------------------------------
+
+# Nam's Method ------------------------------------------------------------
+library(reshape2)
+cor_2 <- round(cor(data2[3:81], use="complete.obs"),2)
+
+
+CM <- cor_2
+CM[lower.tri(CM, diag=TRUE)] <- NA
+cor_predictors <- subset(melt(CM, na.rm=TRUE), value >= abs(.85))
+cor_predictors
+# The next two lines of code tell us the number of unique variables that are correlated with each other.
+length(unique(cor_predictors$Var1))
+length(unique(cor_predictors$Var2))
+
+
+# Lasso Regression --------------------------------------------------------
+
+
+# PCA Regression ----------------------------------------------------------
+
+
+
+# Graham's Method ---------------------------------------------------------
+
+# indices <- which(lower.tri(cor_matrix, diag = FALSE), arr.ind = TRUE)
+#
+# highMC <- matrix()
+#
+# for (idx in 1:nrow(indices)) {
+#   i <- indices[idx, 1]
+#   j <- indices[idx, 2]
+#   if(cor_matrix[i,j] > 0.8){
+#     highMC<-cbind(highMC,cor_matrix[i,j])
+#   }
+# }
+#
+# #Printing values with high MC value
+# print("The values with a high Value of multicollinearity are:")
+# print(highMC)
+
+# Remove values with high VIF from remaining 
+
+# Now we need to drop the columns that are highly correlated with each other. From here, we will need to refit our model. 
+# We'll need to further drop variables as our model is still quite large. We could increase the threshold value as well
 
