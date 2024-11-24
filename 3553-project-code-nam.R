a43acd410fb365928c71878171090b7b3a6d158f
nam-chu <74799165+nam-chu@users.noreply.github.com>
2024-11-23 20:26:27 -0500

    post meeting with chris at library

--- 3553-project-code-nam.R
+++ 3553-project-code-nam.R
@@ -1,6 +1,5 @@
-library(tidyverse) # this is for ggplot2 and all of that stuff
-
-library(readxl) # This library is used to read the excel table in. Prevents the need to convert over to csv as we received the files in xlsx format.
+library(tidyverse)
+library(readxl)
 library(lars)
 library(faraway)
 
@@ -91,7 +90,7 @@ data2 <- data |>
 # I'm rearranging the columns here so that they are listed alphabetically to better match the data dictionary
 data2 <- data2 |> 
   mutate(
-    sex = as.factor(sex), # 
+    sex = as.factor(sex),  
     sport = as.factor(sport)
   ) |> 
   select(order(colnames(data2))) |> 
@@ -103,72 +102,62 @@ rm(data, m_basket, w_basket, m_soc, w_soc, m_hock, w_hock, w_rug, m_foot)
 # Multicollinearity checks ------------------------------------------------
 
 library(reshape2)
-cor_2 <- round(cor(data2[3:81], use="complete.obs"),2)
+cor_2 <- round(cor(data2[3:81], use="complete.obs"), 2)
 
 CM <- cor_2
+
 CM[lower.tri(CM, diag=TRUE)] <- NA
 
-threshold <- 0.30
+threshold <- 0.20
 
 cor_predictors <- subset(melt(CM, na.rm=TRUE), abs(value) <= (threshold))
 
 cor_predictors_2 <- cor_predictors |> 
   filter(Var1 == "Jump Height")
 
+ 
 data3 <- data2 |> 
   select(-c(cor_predictors_2$Var2))
 
 
 # VIF Check ---------------------------------------------------------------
 
-for (i in 1:ncol(data3)) {
-  vif(lm(`Jump Height` ~ data3[,1:i], data=data3))
-}
+# mod_3 <- lm(`Jump Height` ~ sex + sport + data3$`Avg. Landing Force` + data3$`Avg. Braking Force` + data3$`Avg. Braking Power` + data3$`Avg. Braking Velocity`+ data3$`Avg. Propulsive Force` + data3$`Avg. Propulsive Power` +data3$`Avg. Propulsive Velocity` + data3$`Avg. Relative Braking Force` + data3$`Avg. Relative Braking Power` + data3$`Right Avg. Braking Force` + data3$`Takeoff Velocity` + data3$RSI + data3$`Right Force at Peak Propulsive Force` + data3$`Right Force at Peak Landing Force` +data3$`Right Force at Peak Braking Force`+data3$`Right Avg. Propulsive Force`+data3$`Right Avg. Braking RFD`+data3$`Right Avg. Braking Force`+data3$`Relative Propulsive Net Impulse` + data3$`Relative Propulsive Impulse` +data3$`Relative Peak Landing Force`+data3$`Relative Force at Min Displacement`+data3$`Relative Braking Net Impulse`+data3$`Propulsive Net Impulse`+data3$`Peak Velocity`+data3$`Peak Relative Propulsive Power`+data3$`Peak Relative Propulsive Force`+data3$`Peak Relative Braking Power`+data3$`Peak Relative Braking Force`+data3$`Peak Propulsive Power`+data3$`Peak Propulsive Force`+data3$`Peak Landing Force`+data3$`Peak Braking Velocity`+data3$`Peak Braking Power`+data3$`Peak Braking Force`+data3$mRSI+data3$`Jump Momentum`+ data3$`Avg. Relative Propulsive Force`  + data3$`Avg. Relative Propulsive Power` + data3$`Braking Impulse` + data3$`Braking Net Impulse` + data3$`Braking Phase` + data3$`Braking Phase %` + data3$`Braking RFD` + data3$`Countermovement Depth` + data3$`Flight Time` + data3$`Force at Min Displacement` + data3$`Impulse Ratio`, data = data3)
 
-mod_3 <- lm(`Jump Height` ~ sex + sport + data3$`Avg. Landing Force` + data3$`Avg. Braking Force` + data3$`Avg. Braking Power` + data3$`Avg. Braking Velocity`+ data3$`Avg. Propulsive Force` + data3$`Avg. Propulsive Power` +data3$`Avg. Propulsive Velocity` + data3$`Avg. Relative Braking Force` + data3$`Avg. Relative Braking Power` + data3$`Right Avg. Braking Force` + data3$`Takeoff Velocity` + data3$RSI + data3$`Right Force at Peak Propulsive Force` + data3$`Right Force at Peak Landing Force` +data3$`Right Force at Peak Braking Force`+data3$`Right Avg. Propulsive Force`+data3$`Right Avg. Braking RFD`+data3$`Right Avg. Braking Force`+data3$`Relative Propulsive Net Impulse` + data3$`Relative Propulsive Impulse` +data3$`Relative Peak Landing Force`+data3$`Relative Force at Min Displacement`+data3$`Relative Braking Net Impulse`+data3$`Propulsive Net Impulse`+data3$`Peak Velocity`+data3$`Peak Relative Propulsive Power`+data3$`Peak Relative Propulsive Force`+data3$`Peak Relative Braking Power`+data3$`Peak Relative Braking Force`+data3$`Peak Propulsive Power`+data3$`Peak Propulsive Force`+data3$`Peak Landing Force`+data3$`Peak Braking Velocity`+data3$`Peak Braking Power`+data3$`Peak Braking Force`+data3$mRSI+data3$`Jump Momentum`+, data = data3)
 
-mod_3 <- lm(data3$`Jump Height` ~ . - data3$`Left Force at Peak Propulsive Force` - data3$`Left Force at Peak Braking Force` - data3$`Left Avg. Propulsive Force` - data3$`Left Avg. Braking RFD` - data3$`Left Avg. Braking Force`, data = data3)
-# left force at peak propulsive force is perfectly correlated with some other predictors
 
-vif(mod_3)
-alias(mod_4)
+data4 <- data3 |> 
+  select(-c(`Left Force at Peak Propulsive Force`, `Left Force at Peak Braking Force`, `Left Avg. Propulsive Force`, `Left Avg. Braking RFD`, `Left Avg. Braking Force`, `Left Force at Peak Landing Force`))
 
-library(car)  # Ensure the 'car' package is loaded for vif()
+mod_1 <- lm(`Jump Height` ~ . -sex -sport, data = data4)
 
-# Initialize a list to store results
-vif_results <- list()
+vif(mod_1)
 
-# Iterate over each predictor
-for (i in 1:ncol(data3)) {
-  predictor_subset <- colnames(data3)[1:i]  # Get predictor names up to column i
+list_of_vif <- list()
+high_vif <- str_remove_all(names(which.max(vif(mod_1))),"`")
+list_of_vif <- append(list_of_vif , high_vif)
+
+data4 <- data4 |> 
+  select(-c(high_vif))
   
-  # Try to calculate VIF for the current subset
-  tryCatch({
-    # Fit the model
-    model <- lm(`Jump Height` ~ ., data = data3[, c("Jump Height", predictor_subset)])
-    
-    # Calculate VIF and store it
-    vif_results[[i]] <- vif(model)
-  }, error = function(e) {
-    # Print error message and problematic predictors
-    message("Error in iteration ", i, ": ", conditionMessage(e))
-    message("Predictors causing the issue: ", paste(predictor_subset, collapse = ", "))
-  })
+mod_2 <- lm(`Jump Height` ~ . -sex -sport, data = data4)
+
+repeat {
+  vif(mod_2)
+  high_vif <- str_remove_all(names(which.max(vif(mod_2))),"`")
+  list_of_vif <- append(list_of_vif , high_vif)
+  
+  data4 <- data4 |> 
+    select(-c(high_vif))
+  
+  mod_2 <- lm(`Jump Height` ~ . -sex -sport, data = data4)
+  vif(mod_2)
+  
+  if (max(vif(mod_2)) < 4) {
+    break
+  }
 }
-
-# View the VIF results
-vif_results
-
-
-# LASSO  -----------------------------------------------------------------
-
-which(colnames(design_matrix) == "`Jump Height`")
-
-design_matrix <- model.matrix(~ . -1, data = data2)
-
-lmod <- lars(design_matrix[,-27], data2$`Jump Height`)
-lmod
-
+vif(mod_2)
 # Model Diagnostics After MC and LASSO -------------------------------------------------------
 
 mod_1 <- lm(`Jump Height` ~ ., data = data2)
@@ -179,8 +168,8 @@ length(mod_residuals)
 length(mod_fitted)
 
 plot <- ggplot(data2, aes(x = mod_fitted, y = mod_residuals)) +
-                 geom_point() + xlab("Model Fitted Values") +ylab("Model Residuals") + 
-                 geom_hline(yintercept = 0)
+  geom_point() + xlab("Model Fitted Values") +ylab("Model Residuals") + 
+  geom_hline(yintercept = 0)
 plot
 
 # Check the normality assumption ------------------------------------------
@@ -198,6 +187,16 @@ shapiro.test(mod_residuals) # This won't work because Shapiro Test only takes sa
 
 
 # Check for influential points --------------------------------------------
+# LASSO  -----------------------------------------------------------------
+
+design_matrix <- model.matrix(~ . -1, data = data2)
+
+which(colnames(design_matrix) == "`Jump Height`")
+
+
+lmod <- lars(design_matrix[,-27], data2$`Jump Height`)
+min(round(lmod$Cp,2))
+
 
 
 # PCA Test ----------------------------------------------------------------
